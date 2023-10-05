bc06f4633c63a32eb67af0a91a13d678f3f22171
HeniuM <gortag6@gmail.com>
2023-10-05 18:55:40 +0200

    Quick fixes

--- main.py
+++ main.py
@@ -8,15 +8,27 @@ def bmi_calculator():
     if request.method == 'POST':
         weight = float(request.form['weight'])
         height = float(request.form['height'])
+        gender = request.form['gender']
         bmi = calculate_bmi(weight, height)
-        return render_template('result.html', bmi=bmi)
+
+        recommended_bmi_range = get_recommended_bmi_range(gender)
+
+        return render_template('result.html', bmi=bmi, recommended_bmi_range=recommended_bmi_range)
     return render_template('index.html')
 
 
 def calculate_bmi(weight, height):
-    # Calculate BMI
     bmi = weight / (height ** 2)
     return round(bmi, 2)
+
+
+def get_recommended_bmi_range(gender):
+    if gender == 'male':
+        return "Recommended BMI range for men: 18.5 - 24.9"
+    elif gender == 'female':
+        return "Recommended BMI range for women: 18.5 - 24.9"
+    else:
+        return "Recommended BMI range: 18.5 - 24.9"
 
 
 if __name__ == '__main__':
